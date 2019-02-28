a33b16d635ac9571412ce2c3e2eec222a4c00907
cs.park <pcs0331@naver.com>
2019-02-28 16:28:49 +0900

    문제 추가

---
+++ Test/src/programmers/level3/Test5.java
@@ -0,0 +1,66 @@
+package programmers.level3;
+
+import java.util.Arrays;
+
+// 프로그래머스 - 야근 지수(정답은 완료 런타임 오류)
+
+	/*
+	문제 설명
+	
+	회사원 Demi는 가끔은 야근을 하는데요, 야근을 하면 야근 피로도가 쌓입니다. 
+	야근 피로도는 야근을 시작한 시점에서 남은 일의 작업량을 제곱하여 더한 값입니다. 
+	Demi는 N시간 동안 야근 피로도를 최소화하도록 일할 겁니다.Demi가 1시간 동안 작업량 1만큼을 처리할 수 있다고 할 때, 
+	퇴근까지 남은 N 시간과 각 일에 대한 작업량 works에 대해 야근 피로도를 최소화한 값을 리턴하는 함수 solution을 완성해주세요.
+	
+	제한 사항
+	
+	works는 길이 1 이상, 20,000 이하인 배열입니다.
+	works의 원소는 50000 이하인 자연수입니다.
+	n은 1,000,000 이하인 자연수입니다.
+	
+	입출력 예
+	
+	   works	n	result
+	[4, 3, 3]	4	12
+	[2, 1, 2]	1	6
+	[1,1]		3	0
+	
+	입출력 예 설명
+	
+	입출력 예 #1
+	n=4 일 때, 남은 일의 작업량이 [4, 3, 3] 이라면 야근 지수를 최소화하기 위해 4시간동안 일을 한 결과는 [2, 2, 2]입니다. 이 때 야근 지수는 22 + 22 + 22 = 12 입니다.
+	
+	입출력 예 #2
+	n=1일 때, 남은 일의 작업량이 [2,1,2]라면 야근 지수를 최소화하기 위해 1시간동안 일을 한 결과는 [1,1,2]입니다. 야근지수는 12 + 12 + 22 = 6입니다.
+
+	*/
+public class Test5 {
+
+	public static void main(String[] args) {
+		Test5 test5 = new Test5();
+		int[] works = new int[]{1,1};
+		int n = 3;
+		long a = 0;
+		a = test5.solution(n, works);
+		System.out.println(a);
+	}
+	
+    public long solution(int n, int[] works){
+        long answer = 0;
+        
+        Arrays.sort(works);
+        
+        if(n==0 || works[works.length-1] ==0){
+        	for(int i = 0;i<works.length;i++){
+        		answer = answer +works[i]*works[i];
+        	}
+        	return answer;
+        }
+        works[works.length-1]--;
+        n--;
+
+        return solution(n , works);
+
+    }
+
+}
