8c952e6969ed8fbc5df54178886e50af3827b23f
cs.park <pcs0331@naver.com>
2019-04-12 10:28:36 +0900

    enum test

--- Test/src/test/EnumTest.java
+++ Test/src/test/EnumTest.java
@@ -1,39 +1,22 @@
 package test;
 
+import org.junit.Test;
+
 public class EnumTest {
 	public static final String MALE = "MALE";
 	public static final String FEMALE = "FEMALE";
 
 	
 	public static void main(String[] args) {
-		String gender1;
+		Pattern pattern = Pattern.CLUB;
+		System.out.println(pattern.value);
 		
-		gender1 = EnumTest.FEMALE;
-		gender1 = EnumTest.MALE;
-		
-		gender1 = "boy";
-		
-		
-		System.out.println(gender1);
-		
-		Gender gender2;
-		
-		gender2 = Gender.FEMALE;
-		gender2 = Gender.MALE;
-		
-		Drink drink;
-		
-		drink = Drink.AMERICANO;
-		
-		
-		
-		System.out.println(drink.getName());
-		System.out.println(drink.getCost());
-		
-		Pattern pattern=Pattern.CLUB;
-		System.out.println(pattern);
+		Flag flag = Flag.N;
+		System.out.println(flag.getTable1Value());
+		System.out.println(flag.getTable2Value());
 		
 	}
+
 
 	
 	
@@ -63,7 +46,26 @@ enum Gender{
 	MALE,FEMALE;
 }
 
-
+enum Flag{
+	
+	Y("1",true),
+	N("0",false);
+	
+	private String table1Value;
+	private boolean table2Value;
+	
+	Flag(String table1Value, boolean table2Value){
+		this.table1Value = table1Value;
+		this.table2Value = table2Value;
+	}
+	public String getTable1Value() {
+		return table1Value;
+	}
+	
+	public boolean getTable2Value() {
+		return table2Value;
+	}
+}
 
 
 
